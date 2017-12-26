a891dd395a4dfca1390df820de57932fafa34115
Sebastian Sipos <sebastiansipos90@gmail.com>
2017-12-26 20:13:13 +0200

    Add boilerplate.

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+
+import './stylus/app.styl'
+
+import App from './App'
+import router from './router'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
